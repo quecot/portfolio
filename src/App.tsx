9c68e3242505d691103d8b0ae5c411af957c752a
Francesc Vila <francescvilasubias@gmail.com>
2022-11-07 08:46:23 +0100

    fix: link aria-label

--- src/App.tsx
+++ src/App.tsx
@@ -93,18 +93,18 @@ function App() {
         <p>Aquesta és la primera imatge de prova.</p>
       </div>
       <div className="article-title-section article-section">
-        <a href="/" className="project-link">
+        <a aria-label="Link to project" href="/" className="project-link">
           <h2>Primer post de prova</h2>
         </a>
-        <a href="/">
+        <a aria-label="Link to project" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 37.5q-.5 0-.825-.325-.325-.325-.325-.825v-11.2h-11.2q-.5 0-.825-.325Q10.5 24.5 10.5 24q0-.5.325-.825.325-.325.825-.325h11.2v-11.2q0-.5.325-.825.325-.325.825-.325.5 0 .825.325.325.325.325.825v11.2h11.2q.5 0 .825.325.325.325.325.825 0 .5-.325.825-.325.325-.825.325h-11.2v11.2q0 .5-.325.825-.325.325-.825.325Z"/></svg>
         </a>
       </div>
       <div className="article-nav-section article-section">
-        <button name="Previous article" onClick={handleLeftClick}>
+        <button aria-label="Previous article" onClick={handleLeftClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.2 34.5 17.7 25q-.25-.25-.35-.5-.1-.25-.1-.55 0-.3.1-.525.1-.225.35-.475l9.55-9.55q.35-.35.825-.35t.825.35q.3.35.3.85t-.35.85l-8.9 8.85 8.95 8.95q.3.35.3.775 0 .425-.3.825-.35.35-.85.35t-.85-.35Z"/></svg>
         </button>
-        <button name="Next article" onClick={handleRightClick}>
+        <button aria-label="Next article" onClick={handleRightClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M17.95 34.5q-.3-.4-.325-.85-.025-.45.325-.8l8.9-8.9-8.9-8.9q-.35-.3-.325-.8.025-.5.325-.85.4-.35.825-.35.425 0 .775.35l9.55 9.55q.25.25.35.475.1.225.1.525 0 .3-.1.55-.1.25-.35.5l-9.5 9.5q-.35.35-.8.325-.45-.025-.85-.325Z"/></svg>
         </button>
       </div>
@@ -115,18 +115,18 @@ function App() {
         <p>Aquesta és la segona imatge de prova.</p>
       </div>
       <div className="article-title-section article-section">
-        <a href="/" className="project-link">
+        <a aria-label="Link to project" href="/" className="project-link">
           <h2>Segon post de prova</h2>
         </a>
-        <a href="/">
+        <a aria-label="Link to project" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 37.5q-.5 0-.825-.325-.325-.325-.325-.825v-11.2h-11.2q-.5 0-.825-.325Q10.5 24.5 10.5 24q0-.5.325-.825.325-.325.825-.325h11.2v-11.2q0-.5.325-.825.325-.325.825-.325.5 0 .825.325.325.325.325.825v11.2h11.2q.5 0 .825.325.325.325.325.825 0 .5-.325.825-.325.325-.825.325h-11.2v11.2q0 .5-.325.825-.325.325-.825.325Z"/></svg>
         </a>
       </div>
       <div className="article-nav-section article-section">
-        <button name="Previous article" onClick={handleLeftClick}>
+        <button aria-label="Previous article" onClick={handleLeftClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.2 34.5 17.7 25q-.25-.25-.35-.5-.1-.25-.1-.55 0-.3.1-.525.1-.225.35-.475l9.55-9.55q.35-.35.825-.35t.825.35q.3.35.3.85t-.35.85l-8.9 8.85 8.95 8.95q.3.35.3.775 0 .425-.3.825-.35.35-.85.35t-.85-.35Z"/></svg>
         </button>
-        <button name="Next article" onClick={handleRightClick}>
+        <button aria-label="Next article" onClick={handleRightClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M17.95 34.5q-.3-.4-.325-.85-.025-.45.325-.8l8.9-8.9-8.9-8.9q-.35-.3-.325-.8.025-.5.325-.85.4-.35.825-.35.425 0 .775.35l9.55 9.55q.25.25.35.475.1.225.1.525 0 .3-.1.55-.1.25-.35.5l-9.5 9.5q-.35.35-.8.325-.45-.025-.85-.325Z"/></svg>
         </button>
       </div>
@@ -137,18 +137,18 @@ function App() {
         <p>Aquesta és la tercera imatge de prova.</p>
       </div>
       <div className="article-title-section article-section">
-        <a href="/" className="project-link">
+        <a aria-label="Link to project" href="/" className="project-link">
           <h2>Tercer post de prova</h2>
         </a>
-        <a href="/">
+        <a aria-label="Link to project" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 37.5q-.5 0-.825-.325-.325-.325-.325-.825v-11.2h-11.2q-.5 0-.825-.325Q10.5 24.5 10.5 24q0-.5.325-.825.325-.325.825-.325h11.2v-11.2q0-.5.325-.825.325-.325.825-.325.5 0 .825.325.325.325.325.825v11.2h11.2q.5 0 .825.325.325.325.325.825 0 .5-.325.825-.325.325-.825.325h-11.2v11.2q0 .5-.325.825-.325.325-.825.325Z"/></svg>
         </a>
       </div>
       <div className="article-nav-section article-section">
-        <button name="Previous article" onClick={handleLeftClick}>
+        <button aria-label="Previous article" onClick={handleLeftClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.2 34.5 17.7 25q-.25-.25-.35-.5-.1-.25-.1-.55 0-.3.1-.525.1-.225.35-.475l9.55-9.55q.35-.35.825-.35t.825.35q.3.35.3.85t-.35.85l-8.9 8.85 8.95 8.95q.3.35.3.775 0 .425-.3.825-.35.35-.85.35t-.85-.35Z"/></svg>
         </button>
-        <button name="Next article" onClick={handleRightClick}>
+        <button aria-label="Next article" onClick={handleRightClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M17.95 34.5q-.3-.4-.325-.85-.025-.45.325-.8l8.9-8.9-8.9-8.9q-.35-.3-.325-.8.025-.5.325-.85.4-.35.825-.35.425 0 .775.35l9.55 9.55q.25.25.35.475.1.225.1.525 0 .3-.1.55-.1.25-.35.5l-9.5 9.5q-.35.35-.8.325-.45-.025-.85-.325Z"/></svg>
         </button>
       </div>
@@ -159,18 +159,18 @@ function App() {
         <p>Aquesta és la quarta imatge de prova.</p>
       </div>
       <div className="article-title-section article-section">
-        <a href="/" className="project-link">
+        <a aria-label="Link to project" href="/" className="project-link">
           <h2>Quart post de prova</h2>
         </a>
-        <a href="/">
+        <a aria-label="Link to project" href="/">
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 37.5q-.5 0-.825-.325-.325-.325-.325-.825v-11.2h-11.2q-.5 0-.825-.325Q10.5 24.5 10.5 24q0-.5.325-.825.325-.325.825-.325h11.2v-11.2q0-.5.325-.825.325-.325.825-.325.5 0 .825.325.325.325.325.825v11.2h11.2q.5 0 .825.325.325.325.325.825 0 .5-.325.825-.325.325-.825.325h-11.2v11.2q0 .5-.325.825-.325.325-.825.325Z"/></svg>
         </a>
       </div>
       <div className="article-nav-section article-section">
-        <button name="Previous article" onClick={handleLeftClick}>
+        <button aria-label="Previous article" onClick={handleLeftClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M27.2 34.5 17.7 25q-.25-.25-.35-.5-.1-.25-.1-.55 0-.3.1-.525.1-.225.35-.475l9.55-9.55q.35-.35.825-.35t.825.35q.3.35.3.85t-.35.85l-8.9 8.85 8.95 8.95q.3.35.3.775 0 .425-.3.825-.35.35-.85.35t-.85-.35Z"/></svg>
         </button>
-        <button name="Next article" onClick={handleRightClick}>
+        <button aria-label="Next article" onClick={handleRightClick}>
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M17.95 34.5q-.3-.4-.325-.85-.025-.45.325-.8l8.9-8.9-8.9-8.9q-.35-.3-.325-.8.025-.5.325-.85.4-.35.825-.35.425 0 .775.35l9.55 9.55q.25.25.35.475.1.225.1.525 0 .3-.1.55-.1.25-.35.5l-9.5 9.5q-.35.35-.8.325-.45-.025-.85-.325Z"/></svg>
         </button>
       </div>

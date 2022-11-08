28bd3555f9672cbae8df01deea0958250aba0941
Francesc Vila <francescvilasubias@gmail.com>
2022-11-08 09:58:51 +0100

    feat: contact form send email and confirmation using emailjs

--- src/components/contact_page.tsx
+++ src/components/contact_page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactPage:React.FC = () => {
   const openContactMail = () => {
@@ -7,8 +8,41 @@ const ContactPage:React.FC = () => {
     window.open(`mailto:${email}?subject=${subject}`);
   };
 
-  const handleFormSubmit = () => {
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const nameField = (document.querySelector('#form-name') as HTMLInputElement);
+    const emailField = (document.querySelector('#form-email') as HTMLInputElement);
+    const messageField = (document.querySelector('#form-message') as HTMLTextAreaElement);
 
+    const name = nameField.value;
+    const email = emailField.value;
+    const message = messageField.value;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+
+    const emailParams = {
+      name,
+      message,
+      email,
+    };
+
+    const responseParams = {
+      user_name: name,
+      user_email: email,
+    };
+
+    emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, emailParams, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+      .then(() => {
+        emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_RESPONSE_TEMPLATE_ID, responseParams, import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+        nameField.value = '';
+        emailField.value = '';
+        messageField.value = '';
+        nameField.disabled = false;
+        emailField.disabled = false;
+        messageField.disabled = false;
+      });
   };
 
   return (
@@ -50,17 +84,17 @@ const ContactPage:React.FC = () => {
               </h2>
             </div>
             <form id="form" onSubmit={handleFormSubmit}>
-              <div id="form-name" className="form-field">
+              <div className="form-field">
                 <span>NAME</span>
-                <input type="text" required placeholder="What is your name?" />
+                <input id="form-name" type="text" required placeholder="What is your name?" />
               </div>
-              <div id="form-email" className="form-field">
+              <div className="form-field">
                 <span>EMAIL ADDRESS</span>
-                <input type="text" required placeholder="email@example.com" />
+                <input id="form-email" type="text" required placeholder="email@example.com" />
               </div>
-              <div id="form-message" className="form-field">
+              <div className="form-field">
                 <span>MESSAGE</span>
-                <textarea required placeholder="Write your message..." />
+                <textarea id="form-message" required placeholder="Write your message..." />
               </div>
               <button type="submit">Submit</button>
             </form>
